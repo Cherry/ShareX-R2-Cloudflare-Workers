0ee42ba2919a77592b540d5ed033e6237e73cb09
James Ross <james@jross.me>
2024-09-22 22:39:00 +0100

    chore: bump dependencies

--- test/types.ts
+++ test/types.ts
@@ -1,5 +1,6 @@
 import type { Env } from '../src/types';
 
 declare module 'cloudflare:test' {
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 	interface ProvidedEnv extends Env {}
 }
